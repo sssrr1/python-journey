d351e1273a914b32254b33a7451899c3f85bfdea
Rayan <raiyaan.shubhro@gmail.com>
2026-06-25 08:36:05 +0600

    Class 1 completed

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import random
+# ১. ভ্যারিয়েবল ডিক্লেয়ারেশন ও ডেটা অ্যাসাইনমেন্ট
+my_name = "Raiyan"          # String (str)
+my_age = 39                 # Integer (int)
+favorite_language = "Bangla"# String (str)
+is_ready_to_code = True     # Boolean (bool)
 
-# কিছু চেনা শব্দ যা মনে রাখা সহজ
-words = ["baaler", "password", "kothin", "mama", "coding", "python", "safari"]
+# ২. আউটপুট প্রিন্ট করা
+print("Developer Name:", my_name)
+print("Age:", my_age)
+print("Favorite Language:", favorite_language)
+print("Ready to rock?", is_ready_to_code)
 
-# পাইথন নিজে থেকে ৩টি শব্দ বেছে নিয়ে মাঝখানে '-' দিয়ে জোড়া দেবে
-my_passphrase = "-".join(random.choice(words) for i in range(3))
-
-print("Your easy but secure password is:", my_passphrase)
+# ৩. ডেটা টাইপ ভেরিফিকেশন (অপশনাল, যা তুমি করেছ)
+print("\n--- Data Types ---")
+print("my_name type:", type(my_name))
+print("my_age type:", type(my_age))
+print("is_ready_to_code type:", type(is_ready_to_code))
