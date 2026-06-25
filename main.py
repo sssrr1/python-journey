ec91f39f96a390836540fe12f5bdcf1a80ea3c04
Rayan <raiyaan.shubhro@gmail.com>
2026-06-25 07:45:58 +0600

    Password generator added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Git, I am Rayan!")
+import random
+import string
+
+# পাইথনকে বলছি পাসওয়ার্ডে কী কী থাকবে (অক্ষর, সংখ্যা ও চিহ্ন)
+উপাদান = string.ascii_letters + string.digits + string.punctuation
+
+# পাইথন নিজ দায়িত্বে ৮ অক্ষরের একটা কঠিন পাসওয়ার্ড বানাবে
+পাসওয়ার্ড = "".join(random.choice(উপাদান) for i in range(8))
+
+print("মামা, আপনার আজকের সিক্রেট পাসওয়ার্ড হলো:", পাসওয়ার্ড)
